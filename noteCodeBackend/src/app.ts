3ec490ea45cd4f093984bfa62b0608de1d490abc
Jollan <kassegnedisiner1522@gmail.com>
2024-06-21 15:57:17 +0000

    allow origin *

--- noteCodeBackend/src/app.ts
+++ noteCodeBackend/src/app.ts
@@ -6,12 +6,8 @@ import cors from "cors";
 import morgan from "morgan";
 
 const app = express();
-let origin;
-if (process.env.NODE_ENV === "production") {
-  origin = [process.env.ORIGIN_1!];
-} else origin = "http://localhost:4200";
 
-app.use(cors({ origin }));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api/v1/notes", noteRouter);
